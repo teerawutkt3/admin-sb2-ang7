448d7b0b8fb57bf94c3f0831249b551ace47d414
Teerawut <teerawutkt3@gmail.com>
2019-04-23 21:04:42 +0700

    make i

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/sb-admin'));
+app.use(express.static(__dirname + '/dist/sb-admin2-ang7'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/sb-admin/index.html'));
+res.sendFile(path.join(__dirname+'/dist/sb-admin2-ang7/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
